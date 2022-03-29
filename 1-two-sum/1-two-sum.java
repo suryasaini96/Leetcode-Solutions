dc93e5f0cbd37f2af32e4dc89bf4cab063addf22
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-03-29 08:48:00 +0530

    Time: 3 ms (88.45%), Space: 46.4 MB (12.82%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -15,7 +15,7 @@ class Solution {
         throw new IllegalArgumentException("No two solutions exist.");
     }*/
     
-    // Solution 2: Using HashMap
+    // Solution 2: Using HashMap O(n) time complexity
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
