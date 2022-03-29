25f204860fa2346a91735aff6de034a4889e3b66
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-03-29 14:10:51 +0530

    Time: 36 ms (5.11%), Space: 47 MB (5.71%) - LeetHub

---
+++ 179-largest-number/179-largest-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        // Sort the numbers based on which number is greater upon combining
+        // Stream of primitives to stream of objects
+        nums = Arrays.stream(nums)
+            .boxed()
+            .sorted((a,b) -> {
+                String x = a.toString();
+                String y = b.toString();
+                if ((x+y).compareTo(y+x) > 0)
+                    return -1;
+                return 1;
+            }).mapToInt(i -> i.intValue())
+            .toArray();
+        
+        String result = "";
+        for (int x: nums)
+            result += x;
+        
+        return result.replaceFirst("^0+(?!$)", "");
+    }
+}
