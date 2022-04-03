8ffd84c1a09e60e89665c0a9fbf40b638b238e65
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-03 23:01:13 +0530

    Time: 2 ms (65.05%), Space: 42.8 MB (26.76%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,21 +1,41 @@
 class Solution {
     
-    // Using 1-D dp array solution
+    // Most optimal solution -- 100% - Time complexity - O(n) and Space complexity - O(1)
     public int numDecodings(String s) {
-        int n = s.length();
-        int[] dp = new int[n+1];
-        dp[n] = 1;
-        for (int i=n-1; i>=0; i--) {
-            if (s.charAt(i) != '0') {
-                dp[i] = dp[i+1];
-                if (i<n-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7')))
-                    dp[i] += dp[i+2];
-            }
+        if (s.length() == 0 || s.charAt(0) == '0') return 0;
+        if (s.length() == 1) return 1;
+        
+        int count1 = 1; int count2 = 1; 
+        for (int i=1; i<s.length(); i++) {
+            int d = s.charAt(i)-'0';
+            int dd = (s.charAt(i-1)-'0')*10 + d;
+            int count = 0;
+            if (d>0) 
+                count+= count2;
+            if (dd>=10 && dd<=26) 
+                count+= count1;
+            count1 = count2;
+            count2 = count; 
         }
-        return dp[0];
+        return count2;
     }
+         
+    // // Using 1-D dp array solution -- 2ms - 65.05% faster - Space - O(n)
+    // public int numDecodings(String s) {
+    //     int n = s.length();
+    //     int[] dp = new int[n+1];
+    //     dp[n] = 1;
+    //     for (int i=n-1; i>=0; i--) {
+    //         if (s.charAt(i) != '0') {
+    //             dp[i] = dp[i+1];
+    //             if (i<n-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7')))
+    //                 dp[i] += dp[i+2];
+    //         }
+    //     }
+    //     return dp[0];
+    // }
     
-//     // Memoized solution -- 94.83% faster
+//     // Memoized solution -- 1ms - 94.83% faster
 //     public int numDecodings(String s) {
 //         Integer[] memo = new Integer[s.length() + 1];
 //         return s.length()==0?0:numDecodings(0,s,memo);
