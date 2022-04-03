6dc0ee83a667f79cefdd586fe626cfa9bbc78038
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-03 22:36:35 +0530

    Time: 2 ms (65.05%), Space: 42 MB (76.52%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,21 +1,36 @@
 class Solution {
     
-    // Memoization to recursion
+    // Using 1-D dp array solution
     public int numDecodings(String s) {
-        Integer[] memo = new Integer[s.length() + 1];
-        return s.length()==0?0:numDecodings(0,s,memo);
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[n] = 1;
+        for (int i=n-1; i>=0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] = dp[i+1];
+                if (i<n-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7')))
+                    dp[i] += dp[i+2];
+            }
+        }
+        return dp[0];
     }
     
-    private int numDecodings(int i, String s, Integer[] memo) {
-        if (i == s.length()) return 1;
-        if (s.charAt(i) == '0') return 0;
-        if (memo[i]!=null) return memo[i];
-        int ans = numDecodings(i+1, s, memo); // Considering single character -- decode rest of the string
-        if (i<s.length()-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7')))
-            ans+= numDecodings(i+2, s, memo); // Decode rest of the string after 2 combined characters
-        memo[i] = ans;
-        return memo[i];
-    }
+//     // Memoized solution -- 94.83% faster
+//     public int numDecodings(String s) {
+//         Integer[] memo = new Integer[s.length() + 1];
+//         return s.length()==0?0:numDecodings(0,s,memo);
+//     }
+    
+//     private int numDecodings(int i, String s, Integer[] memo) {
+//         if (i == s.length()) return 1;
+//         if (s.charAt(i) == '0') return 0;
+//         if (memo[i]!=null) return memo[i];
+//         int ans = numDecodings(i+1, s, memo); // Considering single character -- decode rest of the string
+//         if (i<s.length()-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7')))
+//             ans+= numDecodings(i+2, s, memo); // Decode rest of the string after 2 combined characters
+//         memo[i] = ans;
+//         return memo[i];
+//     }
     
 //     // Recursive solution - O(2^n) -- TLE
 //     public int numDecodings(String s) {
