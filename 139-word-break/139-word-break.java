d435b961543055a1e467511f3409f79a8365dc93
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-05 00:00:42 +0530

    Time: 14 ms (33.43%), Space: 47.6 MB (22.13%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,22 +1,38 @@
 class Solution {
     
-    // Memoized recursive solution 
-    
-    Boolean memo[];
+    // Bottom-up dp solution
     
     public boolean wordBreak(String s, List<String> wordDict) {
-        memo = new Boolean[s.length()];
-        return wordBreak(0,s,new HashSet(wordDict));            
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[n] = true;
+        Set<String> dict = new HashSet<>(wordDict);
+        for (int i=n-1; i>=0; i--) {
+            for (int j=i+1; j<=n; j++) {
+                if (!dp[i] && dict.contains(s.substring(i,j)))
+                    dp[i] = dp[j];
+            }
+        }
+        return dp[0];
     }
     
-    private boolean wordBreak(int i, String s, HashSet<String> wordDict) {
-        if (i == s.length()) return true;
-        if (memo[i]!=null) return memo[i];
-        for (int j=i+1; j<=s.length(); j++) {
-            String word = s.substring(i,j);
-            if (wordDict.contains(word) && wordBreak(j,s,wordDict))
-                return memo[i]=true;
-        }
-        return memo[i]=false;
-    }
+    // Memoized recursive solution 
+    
+//     Boolean memo[];
+    
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         memo = new Boolean[s.length()];
+//         return wordBreak(0,s,new HashSet(wordDict));            
+//     }
+    
+//     private boolean wordBreak(int i, String s, HashSet<String> wordDict) {
+//         if (i == s.length()) return true;
+//         if (memo[i]!=null) return memo[i];
+//         for (int j=i+1; j<=s.length(); j++) {
+//             String word = s.substring(i,j);
+//             if (wordDict.contains(word) && wordBreak(j,s,wordDict))
+//                 return memo[i]=true;
+//         }
+//         return memo[i]=false;
+//     }
 }
