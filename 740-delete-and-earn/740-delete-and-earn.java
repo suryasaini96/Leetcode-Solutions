3b1259611689df8bccee756b0f3c5174772c1ed0
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-05 22:46:38 +0530

    Time: 7 ms (46.09%), Space: 49.2 MB (6.38%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,40 +1,38 @@
 class Solution {
     
-        
-    
-//     // Using dynamic programming approach
-//     public int deleteAndEarn(int[] nums) {
-//         int[] sum = new int[10001];
-//         int[] dp = new int[10001];
-//         // Store all the total sum of each element in array
-//         for (int num: nums)
-//             sum[num]+= num;
-        
-//         dp[0] = 0;
-//         dp[1] = sum[1];
-//         for (int i=2; i<dp.length; i++) {
-//             dp[i] = Math.max(sum[i] + dp[i-2], dp[i-1]); // Ignore adjacent elements if taking sum[i] else the answer would be dp[i-1]  
-//         }
-//         return dp[dp.length-1];
-//     }
-    
-    // Other dp approach 
+    // Using dynamic programming approach
     public int deleteAndEarn(int[] nums) {
-        int inc = 0; int exc = 0;
-        int freq[] = new int[10001];
+        int[] sum = new int[10001];
+        int[] dp = new int[10001];
+        // Store all the total sum of each element in array
+        for (int num: nums)
+            sum[num]+= num;
         
-        for (int val: nums)
-            freq[val]++;
-        
-        for (int i=0; i<freq.length; i++) {
-            int newInc = exc + freq[i]*i; // Include total num[i]
-            int newExc = Math.max(inc,exc);
-            
-            inc = newInc;
-            exc = newExc;
+        dp[0] = 0;
+        dp[1] = sum[1];
+        for (int i=2; i<dp.length; i++) {
+            dp[i] = Math.max(sum[i] + dp[i-2], dp[i-1]); // Ignore adjacent elements if taking sum[i] else the answer would be dp[i-1]  
         }
-        return Math.max(inc,exc);
-    }    
+        return dp[dp.length-1];
+    }
+    
+//     // Other dp approach 
+//     public int deleteAndEarn(int[] nums) {
+//         int inc = 0; int exc = 0;
+//         int freq[] = new int[10001];
+        
+//         for (int val: nums)
+//             freq[val]++;
+        
+//         for (int i=0; i<freq.length; i++) {
+//             int newInc = exc + freq[i]*i; // Include total num[i]
+//             int newExc = Math.max(inc,exc);
+            
+//             inc = newInc;
+//             exc = newExc;
+//         }
+//         return Math.max(inc,exc);
+//     }    
             
         
     
