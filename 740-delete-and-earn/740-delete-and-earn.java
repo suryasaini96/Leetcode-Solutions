b807d87cea4b0f08e3cd9c6dc3dde36bfaafcdee
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-05 22:45:52 +0530

    Time: 5 ms (66.76%), Space: 47.7 MB (16.65%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,5 +1,7 @@
 class Solution {
     
+        
+    
 //     // Using dynamic programming approach
 //     public int deleteAndEarn(int[] nums) {
 //         int[] sum = new int[10001];
@@ -11,44 +13,63 @@ class Solution {
 //         dp[0] = 0;
 //         dp[1] = sum[1];
 //         for (int i=2; i<dp.length; i++) {
-//             dp[i] = Math.max(sum[i] + dp[i-2], dp[i-1]);
+//             dp[i] = Math.max(sum[i] + dp[i-2], dp[i-1]); // Ignore adjacent elements if taking sum[i] else the answer would be dp[i-1]  
 //         }
 //         return dp[dp.length-1];
 //     }
-        
     
-    // Recursive solution with memoization
-    Integer[] memo = new Integer[20001];
-    
+    // Other dp approach 
     public int deleteAndEarn(int[] nums) {
-        // Sort the array first so we don't have to include nums[i]-1 element
-        Arrays.sort(nums);
-        return solve(nums, 0); 
-    }
-    
-    private int solve(int[] nums, int i) {
-        if (i >= nums.length)
-            return 0;
+        int inc = 0; int exc = 0;
+        int freq[] = new int[10001];
         
-        if (memo[i]!=null)
-            return memo[i];
+        for (int val: nums)
+            freq[val]++;
         
-        int currValue = nums[i];
-        int currSum = nums[i];
-        int index = i+1;
-        
-        // Take all curr elements
-        while (index < nums.length && nums[index] == currValue)
-            currSum+= nums[index++];
-        
-        int remIndex = index;
-        // Now skip curr + 1 elements
-        while (index < nums.length && nums[index] == currValue + 1)
-            index++;
-        
-        // Either include currSum and solve for rest
-        // or exclude currValue and solve for remaining values 
-        return memo[i] = Math.max(currSum + solve(nums, index), solve(nums, remIndex));
+        for (int i=0; i<freq.length; i++) {
+            int newInc = exc + freq[i]*i; // Include total num[i]
+            int newExc = Math.max(inc,exc);
             
-    }
+            inc = newInc;
+            exc = newExc;
+        }
+        return Math.max(inc,exc);
+    }    
+            
+        
+    
+//     // Recursive solution with memoization
+//     Integer[] memo = new Integer[20001];
+    
+//     public int deleteAndEarn(int[] nums) {
+//         // Sort the array first so we don't have to include nums[i]-1 element
+//         Arrays.sort(nums);
+//         return solve(nums, 0); 
+//     }
+    
+//     private int solve(int[] nums, int i) {
+//         if (i >= nums.length)
+//             return 0;
+        
+//         if (memo[i]!=null)
+//             return memo[i];
+        
+//         int currValue = nums[i];
+//         int currSum = nums[i];
+//         int index = i+1;
+        
+//         // Take all curr elements
+//         while (index < nums.length && nums[index] == currValue)
+//             currSum+= nums[index++];
+        
+//         int remIndex = index;
+//         // Now skip curr + 1 elements
+//         while (index < nums.length && nums[index] == currValue + 1)
+//             index++;
+        
+//         // Either include currSum and solve for rest
+//         // or exclude currValue and solve for remaining values 
+//         return memo[i] = Math.max(currSum + solve(nums, index), solve(nums, remIndex));
+            
+//     }
 }
