77c0f7fdc2f0917fd5856d6b540a6f5eee93f118
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-10 20:38:45 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (74.23%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
         if (nums.length == 1) return nums[0];
-        int dp[] = new int[nums.length]; // Will store the loot till nth house
+        int dp[] = new int[nums.length]; // Will store the max loot till nth house
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
         for (int i=2; i<nums.length; i++) {
