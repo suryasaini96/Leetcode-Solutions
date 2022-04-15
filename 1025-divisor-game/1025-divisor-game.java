e16d3df3fedbed2180751ed655aa87f2afdd41f9
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-15 20:12:22 +0530

    Time: 1 ms (21.94%), Space: 39 MB (89.98%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,29 +1,9 @@
 class Solution {
-    // public boolean divisorGame(int n) {
-    //     return n%2 == 0;
-    // }
     
-    // Bottom up dp solution
-    // public boolean divisorGame(int n) {
-    //     boolean dp[] = new boolean[n+1];
-    //     dp[0] = false;
-    //     dp[1] = false;
-    //     for (int i=2; i<=n; i++) {
-    //         for (int x=1; x<i; x++) { // Finding factors of n
-    //             if (i%x==0) {
-    //                 if (dp[i-x] == false) { // If bob loses on n-x then chose that x and set dp[i] as true
-    //                     dp[i] = true;
-    //                     break;
-    //                 }
-    //             }      
-    //         }
-    //     }
-    //     return dp[n];
-    // }
-    
+    // Bottom up - dp solution
     public boolean divisorGame(int n) {
         boolean dp[] = new boolean[n+1];
-        dp[0] = true;
+        dp[0] = true; // Don't want to execute condition on n==0 as it can never happen
         dp[1] = false;
         for (int i=2; i<=n; i++) {
             for (int x=1; x*x<=i; x++) { // Finding factors of n
@@ -70,5 +50,11 @@ class Solution {
     //             return true;
     //     }
     //     return false;
-    // }    
+    // }
+    
+    
+    // Mathematical solution
+    // public boolean divisorGame(int n) {
+    //     return n%2 == 0;
+    // }
 }
