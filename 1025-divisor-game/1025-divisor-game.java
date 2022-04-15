de063938e176a8b2a24eaf65aa0bf7240e7a8a1b
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-15 20:05:08 +0530

    Time: 0 ms (100.00%), Space: 39 MB (89.98%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -3,18 +3,43 @@ class Solution {
     //     return n%2 == 0;
     // }
     
+    // public boolean divisorGame(int n) {
+    //     boolean dp[] = new boolean[n+1];
+    //     dp[0] = false;
+    //     dp[1] = false;
+    //     for (int i=2; i<=n; i++) {
+    //         for (int x=1; x<i; x++) { // Finding factors of n
+    //             if (i%x==0) {
+    //                 if (dp[i-x] == false) { // If bob loses on n-x then chose that x and set dp[i] as true
+    //                     dp[i] = true;
+    //                     break;
+    //                 }
+    //             }      
+    //         }
+    //     }
+    //     return dp[n];
+    // }
+    
+    
+    Boolean dp[];
     public boolean divisorGame(int n) {
-        boolean dp[] = new boolean[n+1];
-        dp[0] = false;
+        if (n==0 || n==1) return false;
+        dp = new Boolean[n+1];
+        dp[0] = true; // n can never be 0 so set to true so that recursive call doesn't run
         dp[1] = false;
-        for (int i=2; i<=n; i++) {
-            for (int x=1; x<i; x++) { // Finding factors of n
-                if (i%x==0) {
-                    if (dp[i-x] == false) { // If bob loses on n-x then chose that x and set dp[i] as true
-                        dp[i] = true;
-                        break;
-                    }
-                }      
+        boolean result = helper(n);
+        return result;
+    }
+    
+    private boolean helper(int n) {
+        if (dp[n]!=null) return dp[n];
+        dp[n] = false;
+        for (int x=1; x*x<=n; x++) {
+            if (n%x == 0) {
+               if (!helper(n-x) || !helper(n - (n/x))) { // Found a way where Bob will lose so alice must choose that optimal way
+                   dp[n] = true;
+                   break;
+               } 
             }
         }
         return dp[n];
