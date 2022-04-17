a7c6559b426eed12d26fd6d329978627c28e3352
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-17 15:46:46 +0530

    Time: 1 ms (90.70%), Space: 43.1 MB (82.22%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -2,26 +2,27 @@ class Solution {
     public int trap(int[] height) {
         int maxIndex = 0;
         int maxElement = height[0];
+        // Find the peak value and it's index
         for (int i=0; i<height.length; i++) {
             if (height[i] > maxElement) {
                 maxIndex = i; 
                 maxElement = height[i];
             }   
         }
-        return captureLeft(height, maxIndex) + captureRight(height, maxIndex);
+        return captureLeft(height, maxIndex) + captureRight(height, maxIndex); // Capture left and capture right
      }
     
-    private int captureLeft(int[] height, int maxIndex) {
+    private int captureLeft(int[] height, int maxIndex) { // Capture water from 0th index till peak value index
         int capturedWater = 0;
         for (int i=0; i<=maxIndex; i++) {
             int curr = height[i];
             int j=i+1;
             int currSum = 0;
             while (j <= maxIndex && height[j] < curr) {
-                currSum+= curr - height[j]; // Capture current water
+                currSum+= curr - height[j]; // Capture current water 
                 j++;
             }
-            if (j <= maxIndex && height[j] >= curr) {
+            if (j <= maxIndex && height[j] >= curr) { // When height is larger or equal than curr: set i = j and add captured water
                 i = j - 1;
                 capturedWater+= currSum;
             }
@@ -29,17 +30,17 @@ class Solution {
         return capturedWater;
     }
     
-    private int captureRight(int[] height, int maxIndex) {
+    private int captureRight(int[] height, int maxIndex) { // Capture water from nth index till peak value index
         int capturedWater = 0;
          for (int i=height.length-1; i>=maxIndex; i--) {
              int curr = height[i];
              int j = i-1;
              int currSum = 0;
              while (j >= maxIndex && height[j] < curr) {
-                 currSum+= curr - height[j];
+                 currSum+= curr - height[j]; // Capture current water
                  j--;
              }
-             if (j >= maxIndex && height[j] >= curr) {
+             if (j >= maxIndex && height[j] >= curr) { // When height is larger than or equal to curr: set i = j and add captured water
                  i = j + 1;
                  capturedWater+= currSum;
              }
