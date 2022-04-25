66d5a0a44f3bc005bdee84e93f820f3882bf6588
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-04-25 18:02:35 +0530

    Time: 21 ms (37.84%), Space: 46.7 MB (29.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,21 +1,35 @@
 class Solution {
     
-    // Using top-down memoized approach
+    // Using bottom-up approach
     public int longestCommonSubsequence(String text1, String text2) {
-        Integer[][] memo = new Integer[text1.length()][text2.length()];
-        return helper(text1, text2, 0, 0, memo);
+        int dp[][] = new int[text1.length()+1][text2.length()+1];
+        for (int i=1; i<text1.length()+1; i++) {
+            for (int j=1; j<text2.length()+1; j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[text1.length()][text2.length()];
     }
     
-    private int helper(String a, String b, int i, int j, Integer[][] memo) {
-        if (i >= a.length() || j >= b.length())
-            return 0;
-        if (memo[i][j] != null)
-            return memo[i][j];
+//     // Using top-down memoized approach
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         Integer[][] memo = new Integer[text1.length()][text2.length()];
+//         return helper(text1, text2, 0, 0, memo);
+//     }
+    
+//     private int helper(String a, String b, int i, int j, Integer[][] memo) {
+//         if (i >= a.length() || j >= b.length())
+//             return 0;
+//         if (memo[i][j] != null)
+//             return memo[i][j];
         
-        if (a.charAt(i) == b.charAt(j))
-            return memo[i][j] = 1 + helper(a, b, i+1, j+1, memo);
-        else
-            return memo[i][j] = Math.max(helper(a, b, i+1, j, memo), helper(a, b, i, j+1, memo));
-    }
+//         if (a.charAt(i) == b.charAt(j))
+//             return memo[i][j] = 1 + helper(a, b, i+1, j+1, memo);
+//         else
+//             return memo[i][j] = Math.max(helper(a, b, i+1, j, memo), helper(a, b, i, j+1, memo));
+//     }
     
 }
