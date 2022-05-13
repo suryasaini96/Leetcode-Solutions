1621a452b1d0f62c0b6148048aa534e3d1f64197
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-05-14 03:01:24 +0530

    Time: 7 ms (70.24%), Space: 44.6 MB (22.88%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,23 +1,22 @@
 class Solution {
     
-    public String reverseWords(String s) {
-        List<String> words = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i<s.length(); i++) {
-            if (Character.isAlphabetic(s.charAt(i)) || Character.isDigit(s.charAt(i)))
-                sb.append(s.charAt(i)); 
-            else {
-                if (sb.length()>0)
-                    words.add(sb.toString());
-                sb.setLength(0);
-            }
-        }
-        if (sb.length()>0)
-            words.add(sb.toString());
-        Collections.reverse(words);
-        return String.join(" ", words);
-        
-    }
+//     public String reverseWords(String s) {
+//         List<String> words = new ArrayList<>();
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i<s.length(); i++) {
+//             if (Character.isAlphabetic(s.charAt(i)) || Character.isDigit(s.charAt(i)))
+//                 sb.append(s.charAt(i)); 
+//             else {
+//                 if (sb.length()>0)
+//                     words.add(sb.toString());
+//                 sb.setLength(0);
+//             }
+//         }
+//         if (sb.length()>0)
+//             words.add(sb.toString());
+//         Collections.reverse(words);
+//         return String.join(" ", words);  
+//     }
     
     
     // public String reverseWords(String s) {
@@ -42,4 +41,14 @@ class Solution {
     //     Collections.reverse(wordsWithoutSpaces);
     //     return String.join(" ", wordsWithoutSpaces);
     // }
+    
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length-1; i>=0; i--) {
+            if (!words[i].isEmpty())
+                sb.append(words[i] + " ");
+        }
+        return sb.toString().trim();
+    }
 }
