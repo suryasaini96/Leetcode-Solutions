a0178442834b47912dac96db03b828fd70e9c800
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-05-21 18:30:30 +0530

    Time: 2 ms (52.12%), Space: 41.1 MB (63.02%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        while (n%2==0) n/=2;
+        if (n == 1) return true;
+        return false;
+    }
+}
