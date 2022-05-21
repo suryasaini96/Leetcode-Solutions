e3ae84e0c995b4f746a2926e33a029fa18957dd9
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-05-21 17:11:41 +0530

    Time: 0 ms (100.00%), Space: 50.1 MB (6.75%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,33 +1,47 @@
 class Solution {
     
-    
-    
-    // Approach 1: Using binary search
+    // O(m+n) solution
     public int countNegatives(int[][] grid) {
-        int sum = 0;
-        for (int i = 0; i < grid.length; i ++) {
-            int pivot = firstNegative(grid[i], 0, grid[i].length-1);
-            if (pivot == -1)
-                sum += 0;
-            else
-                sum += grid[i].length - pivot;
-        }
-        return sum;
-    }
-    
-    private int firstNegative(int[] nums, int left, int right) {
-        int pivot = -1;
-        while (left <= right) {
-            int mid = left + (right - left)/2;
-            if (nums[mid] < 0)
-                right = mid;
-            else
-                left = mid+1;
-            if (left == right && nums[right] < 0) {
-                pivot = right;
-                break;
+        int loopStart = grid[0].length-1;
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = loopStart; j>=-1; j--) {
+                if (j == -1 || grid[i][j] >= 0) {
+                    count += grid[0].length - j - 1;
+                    loopStart = j;
+                    break;
+                }
             }
         }
-        return pivot;
+        return count;
     }
+    
+//     // Approach 1: Using binary search
+//     public int countNegatives(int[][] grid) {
+//         int sum = 0;
+//         for (int i = 0; i < grid.length; i ++) {
+//             int pivot = firstNegative(grid[i], 0, grid[i].length-1);
+//             if (pivot == -1)
+//                 sum += 0;
+//             else
+//                 sum += grid[i].length - pivot;
+//         }
+//         return sum;
+//     }
+    
+//     private int firstNegative(int[] nums, int left, int right) {
+//         int pivot = -1;
+//         while (left <= right) {
+//             int mid = left + (right - left)/2;
+//             if (nums[mid] < 0)
+//                 right = mid;
+//             else
+//                 left = mid+1;
+//             if (left == right && nums[right] < 0) {
+//                 pivot = right;
+//                 break;
+//             }
+//         }
+//         return pivot;
+//     }
 }
