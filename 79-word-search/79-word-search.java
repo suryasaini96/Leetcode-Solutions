4e5f27a2521e34a4eb18dc5950aa2d610d7771bb
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-05-23 20:57:50 +0530

    Time: 138 ms (62.86%), Space: 41.8 MB (70.25%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    boolean result = dfs(word, 0, i, j, board, new boolean[board.length][board[0].length]);
+                    if (!result)
+                        continue;
+                    else
+                        return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean dfs(String word, int pos, int i, int j, char[][] board, boolean visited[][]) {
+        
+        if (pos >= word.length())
+            return true;
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j])
+            return false;
+            
+        if (board[i][j] == word.charAt(pos)) {
+            visited[i][j] = true;
+            boolean result = dfs(word, pos+1, i-1, j, board, visited) || dfs(word, pos+1, i, j+1, board, visited) 
+                || dfs(word, pos+1, i+1, j, board, visited) || dfs(word, pos+1, i, j-1, board, visited);
+            if (!result)
+                return visited[i][j] = false;
+            else
+                return true;
+        } else {
+            visited[i][j] = false;
+            return false;
+        }
+    }
+}
