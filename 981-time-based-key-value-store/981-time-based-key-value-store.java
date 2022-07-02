43022c2d458244497bdc261a652309b926e5b093
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-07-02 19:37:18 +0530

    Time: 230 ms (79.13%), Space: 219.5 MB (34.34%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,29 +1,81 @@
 class TimeMap {
     
+    // TreeMap Solution 
+    
     // key, timestamp, value
-    private Map<String, TreeMap<Integer, String>> map;
+//     private Map<String, TreeMap<Integer, String>> map;
+    
+//     public TimeMap() {
+//         map = new HashMap<>();    
+//     }
+    
+//     public void set(String key, String value, int timestamp) {
+//         if (!map.containsKey(key)) {
+//             map.put(key, new TreeMap<Integer, String>());
+//         }
+//         map.get(key).put(timestamp, value);
+//     }
+    
+//     public String get(String key, int timestamp) {
+//         TreeMap<Integer, String> treeMap = map.get(key);
+//         if (treeMap == null) { // No mapping for the key
+//             return "";
+//         }
+//         Integer floorKey = treeMap.floorKey(timestamp);
+//         if (floorKey == null) { // No timestamp lower than given timestamp exists for given key
+//             return "";
+//         }
+//         return treeMap.get(floorKey);
+//     }
+    
+    // Binary Search Solution
+    
+    private Map<String, List<Data>> map;
     
     public TimeMap() {
-        map = new HashMap<>();    
+        map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
         if (!map.containsKey(key)) {
-            map.put(key, new TreeMap<Integer, String>());
+            map.put(key, new ArrayList<>());
         }
-        map.get(key).put(timestamp, value);
+        map.get(key).add(new Data(timestamp, value));
     }
     
     public String get(String key, int timestamp) {
-        TreeMap<Integer, String> treeMap = map.get(key);
-        if (treeMap == null) { // No mapping for the key
+        if (!map.containsKey(key))
             return "";
+        return binarySearch(map.get(key), timestamp);
+    }
+    
+    private String binarySearch(List<Data> list, int timestamp) {
+         // Apply binary search on List<Data>
+        int low = 0, high = list.size() - 1;
+        int index = -1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (list.get(mid).time <= timestamp) {
+                // Make sure the highest time is returned from the time less than timestamp
+                index = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        Integer floorKey = treeMap.floorKey(timestamp);
-        if (floorKey == null) { // No timestamp lower than given timestamp exists for given key
-            return "";
-        }
-        return treeMap.get(floorKey);
+        return index != -1 ? list.get(index).value : "";
+    }
+    
+}
+
+
+class Data {
+    Integer time;
+    String value;
+    
+    Data(Integer time, String value) {
+        this.time = time;
+        this.value = value;
     }
 }
 
