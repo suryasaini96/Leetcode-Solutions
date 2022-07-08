1e4ab63fcf0a3a7cc25b67a902ba4b96d0b7360a
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-07-08 22:19:04 +0530

    Time: 252 ms (14.20%), Space: 134.6 MB (11.53%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        // Exactly same as NGE in stack
+        Stack<Integer> stack = new Stack<>();
+        int[] days = new int[temperatures.length];
+        for (int i = temperatures.length - 1; i>=0; i--) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                days[i] = stack.peek() - i;
+            } else {
+                days[i] = 0;
+            }
+            stack.push(i);
+        }
+        return days;
+    }
+}
