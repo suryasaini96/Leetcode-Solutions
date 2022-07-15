5cee8e8bf4cb39d59637f80f97f4c5b2e95ea011
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-07-15 20:37:57 +0530

    Time: 1 ms (79.72%), Space: 45 MB (74.26%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -14,25 +14,53 @@
  * }
  */
 class Solution {
+    
+    int count = 0;
+    int kthMin = 0;
+
     public int kthSmallest(TreeNode root, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-        helper(root, k, pq);
-        return pq.poll();
+        count = k;
+        helper(root, k);
+        return kthMin;
     }
     
-    private void helper(TreeNode root, int k, PriorityQueue<Integer> pq) {
+    private void helper(TreeNode root, int k) {
         if (root == null) {
             return;
         }
-        
-        if (pq.size() == k && root.val < pq.peek()) {
-            pq.poll();
-            pq.add(root.val);
-        } else if (pq.size() < k) {
-            pq.add(root.val);
+        if (root.left != null) {
+            helper(root.left, k);
         }
-        
-        helper(root.left, k, pq);
-        helper(root.right, k, pq);
+        count--;
+        if (count == 0) {
+            kthMin = root.val;
+            return;
+        }
+        if (root.right != null) {
+            helper(root.right, k);
+        }
     }
+    
+    // Using priority queue: O(log(n))
+//     public int kthSmallest(TreeNode root, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+//         helper(root, k, pq);
+//         return pq.poll();
+//     }
+    
+//     private void helper(TreeNode root, int k, PriorityQueue<Integer> pq) {
+//         if (root == null) {
+//             return;
+//         }
+        
+//         if (pq.size() == k && root.val < pq.peek()) {
+//             pq.poll();
+//             pq.add(root.val);
+//         } else if (pq.size() < k) {
+//             pq.add(root.val);
+//         }
+        
+//         helper(root.left, k, pq);
+//         helper(root.right, k, pq);
+//     }
 }
