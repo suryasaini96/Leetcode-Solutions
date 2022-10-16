180a2dee1698d59fe4498f0ef1afdc79557ef872
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-10-16 20:03:34 +0530

    Time: 24 ms (74.12%), Space: 54 MB (80.85%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,37 +1,24 @@
 class Solution {
     public int maxDistance(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int max = -1, INF = Integer.MAX_VALUE;
+        int max = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) continue;
-                int top = INF, left = INF;
-                if (i-1 >= 0)
-                    top = grid[i-1][j];
-                if (j-1 >= 0)
-                    left = grid[i][j-1];
-                
-                if (top == INF && left == INF)
-                    grid[i][j] = INF;
-                else
-                    grid[i][j] = Math.min(top, left) + 1;
+                grid[i][j] = 201;
+                if (i > 0) grid[i][j] = Math.min(grid[i][j], grid[i-1][j] + 1);
+                if (j > 0) grid[i][j] = Math.min(grid[i][j], grid[i][j-1] + 1);
             }
         }
         
         for (int i = m-1; i >= 0; i--) {
             for (int j = n-1; j >= 0; j--) {
                 if (grid[i][j] == 1) continue;
-                int right = INF, down = INF;
-                if (i + 1 < m)
-                    down = grid[i+1][j];
-                if (j + 1 < n)
-                    right = grid[i][j+1];
-                int min = Math.min(right, down);
-                min = min == INF ? INF : min + 1;
-                grid[i][j] = Math.min(grid[i][j], min);
+                if (i + 1 < m) grid[i][j] = Math.min(grid[i][j], grid[i+1][j] + 1);
+                if (j + 1 < n) grid[i][j] = Math.min(grid[i][j], grid[i][j+1] + 1);
                 max = Math.max(max, grid[i][j]);
             }
         }
-        return (max == Integer.MAX_VALUE || max == -1) ? -1 : max-1;
+        return max == 201 ? -1 : max - 1;
     }
 }
