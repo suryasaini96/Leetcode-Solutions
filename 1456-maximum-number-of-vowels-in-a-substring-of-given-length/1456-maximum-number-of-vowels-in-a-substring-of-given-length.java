9c89841b3cc01392772c8250425934ed967a4f1d
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2022-12-17 23:01:30 +0530

    Time: 67 ms (15.85%), Space: 48.2 MB (34.97%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,34 +1,16 @@
 class Solution {
     
-    private static Set<Character> vowels = new HashSet<>();
-    
-    static {
-        vowels.add('a'); vowels.add('e'); vowels.add('i');
-        vowels.add('o'); vowels.add('u');
-    }
-    
     public int maxVowels(String s, int k) {
-        Deque<Character> q = new LinkedList<Character>();
-        int j = 0, vowelCount = 0, maxCount = 0;
-        while (j < s.length()) {
-            char c = s.charAt(j);
-            if (q.size() < k) {
-                if (vowels.contains(c)) {
-                    vowelCount++;
-                }
-                q.add(c);
-            } else { // When q size is equal to k
-                char firstChar = q.pollFirst();
-                if (vowels.contains(firstChar)) {
-                    vowelCount--;
-                }
-                if (vowels.contains(c)) {
-                    vowelCount++;
-                }
-                q.add(c);
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        int maxCount = 0;
+        for (int i = 0, count = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                count++;
             }
-            maxCount = Math.max(maxCount, vowelCount);
-            j++;
+            if (i >= k && vowels.contains(s.charAt(i-k))) {
+                count--;
+            }
+            maxCount = Math.max(maxCount, count);
         }
         return maxCount;
     }
