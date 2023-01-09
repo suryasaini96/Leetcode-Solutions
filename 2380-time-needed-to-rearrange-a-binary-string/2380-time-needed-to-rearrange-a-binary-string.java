f38784963ed952c5a5eff45daba17546e26b533b
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2023-01-09 21:50:38 +0530

    Time: 824 ms (14.21%), Space: 43 MB (33.58%) - LeetHub

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int secondsToRemoveOccurrences(String s) {
+        int time = 0;
+        while (s.indexOf("01") != -1) {
+            s = s.replaceAll("01", "10");
+            time++;
+        }
+        return time;
+    }
+}
