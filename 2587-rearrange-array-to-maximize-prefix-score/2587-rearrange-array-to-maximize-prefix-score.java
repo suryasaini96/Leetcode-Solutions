58944dc6ce7ebf96735b0508b14de98d1b3ee6f5
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2023-07-08 23:20:01 +0530

    Time: 34 ms (23.08%), Space: 56.2 MB (69.82%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
@@ -1,32 +1,17 @@
 class Solution {
     public int maxScore(int[] nums) {
-        int len = nums.length, k = 0, MOD = 1000000007;
+        int len = nums.length, MOD = 1000000007;
+        Arrays.sort(nums);
         long[] prefix = new long[len];
-        ArrayList<Integer> pos = new ArrayList<>();
-        ArrayList<Integer> neg = new ArrayList<>();
-        ArrayList<Integer> zero = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
-            if (nums[i] < 0) neg.add(nums[i]);
-            else if (nums[i] == 0) zero.add(nums[i]);
-            else if (nums[i] > 0) pos.add(nums[i]);
-        }
-        Collections.sort(neg, (a, b) -> b - a);
-        for (int i = 0; i < pos.size(); i++) {
-            prefix[k++] = pos.get(i); 
-        }
-        for (int i = 0; i < zero.size(); i++) {
-            prefix[k++] = zero.get(i); 
-        }
-        for (int i = 0; i < neg.size(); i++) {
-            prefix[k++] = neg.get(i); 
-        }
-        
-        int score = prefix[0] > 0 ? 1 : 0;
-        prefix[0] = prefix[0] % MOD;
-        for (int i = 1; i < len; i++) {
-            prefix[i] = prefix[i] % MOD + prefix[i-1];
-            if (prefix[i] > 0)
+        prefix[len-1] = nums[len-1] % MOD;
+        int score = prefix[len-1] > 0 ? 1 : 0;
+        for (int i = len-2; i>=0; i--) {
+            prefix[i] = nums[i] % MOD + prefix[i+1];
+            if (prefix[i] > 0) {
                 score++;
+            } else {
+                break;
+            }
         }
         return score;
     }
