dc5c7ec3ece2b11f8f79d439745d970b206c569c
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2023-07-16 00:01:05 +0530

    Time: 26 ms (67.58%), Space: 61.8 MB (59.77%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -1,58 +1,21 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
         int n = garbage.length, mIndex = -1, pIndex = -1, gIndex = -1, time = 0;
-        for (int i = n - 1; i >= 0; i--) { // Set last indices of P, M, G
+        for (int i = 0; i <= n - 1; i++) { // Set last indices of P, M, G
             String g = garbage[i];
-            if (mIndex == -1 && g.contains("M"))
-                mIndex = i;
-            if (pIndex == -1 && g.contains("P"))
-                pIndex = i;
-            if (gIndex == -1 && g.contains("G"))
-                gIndex = i;       
+            time += g.length(); // Collect all garbage
+            if (g.contains("M")) mIndex = i;
+            if (g.contains("P")) pIndex = i;
+            if (g.contains("G")) gIndex = i;
+        }
+        for (int i = 1; i < travel.length; i++) {
+            travel[i] += travel[i-1]; // Prefix sum all travel time to reach index i
         }
         
-        // Collect all P
-        for (int i = 0; i <= pIndex; i++) {
-            String g = garbage[i];
-            if (g.contains("P")) {
-                int collectTime = getCount(g, 'P');
-                time += collectTime;
-            }
-            if (i > 0)
-                time += travel[i-1];  
-        }
-        
-        // Collect all M
-        for (int i = 0; i <= mIndex; i++) {
-            String g = garbage[i];
-            if (g.contains("M")) {
-                int collectTime = getCount(g, 'M');
-                time += collectTime;
-            }
-            if (i > 0)
-                time += travel[i-1]; 
-        }
-        
-        // Collect all G
-        for (int i = 0; i <= gIndex; i++) {
-            String g = garbage[i];
-            if (g.contains("G")) {
-                int collectTime = getCount(g, 'G');
-                time += collectTime;
-            }
-            if (i > 0)
-                time += travel[i-1];  
-        }
-        return time;  
+        time += mIndex >=1 ? travel[mIndex-1] : 0; // Sum travel time to reach last M index
+        time += pIndex >=1 ? travel[pIndex-1] : 0; // Sum travel time to reach last P index
+        time += gIndex >=1 ? travel[gIndex-1] : 0; // Sum travel time to reach last G index
+        return time;
     }
-    
-    public int getCount(String g, char c) {
-        int count = 0;
-        for (int i = 0; i < g.length(); i++) {
-            if (g.charAt(i) == c) {
-                count++;
-            }
-        }
-        return count;
-    }
+
 }
