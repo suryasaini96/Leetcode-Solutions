d56840ba07c7381555136b6e74a6e3f956445bff
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2024-03-24 16:12:02 +0530

    Time: 24 ms (93.64%), Space: 42.9 MB (21.58%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,51 +19,17 @@ class Node {
 */
 
 class Solution {
+
+    public HashMap<Integer, Node> map = new HashMap<>();
+
     public Node cloneGraph(Node node) {
-        boolean[] vis = new boolean[101];
-        boolean[] updateVis = new boolean[101];
-        HashMap<Node, Node> newNodeMap = new HashMap<>();
-        Node newHead = dfs(node, vis, newNodeMap);
-        updateList(node, updateVis, newNodeMap);
-        return newHead;
-    }
-    
-    public Node dfs(Node node, boolean[] vis, HashMap<Node, Node> newNodeMap) {
-        if (node == null || vis[node.val]) return null;
-        int value = node.val;
-        vis[value] = true;
-        List<Node> neighbors = node.neighbors;
-        Node newNode = new Node(node.val);
-        newNodeMap.put(node, newNode);
-        for (Node adj : neighbors) {
-            dfs(adj, vis, newNodeMap);
+        if (node == null) return null;
+        if (map.containsKey(node.val)) return map.get(node.val);
+        Node newNode = new Node(node.val, new ArrayList<Node>());
+        map.put(node.val, newNode);
+        for (Node neighbor : node.neighbors) {
+            newNode.neighbors.add(cloneGraph(neighbor));
         }
         return newNode;
     }
-    
-    public void updateAdjList(Node node, HashMap<Node, Node> newNodeMap) {
-        if (node == null) return;
-        Node newNode = newNodeMap.get(node);
-        List<Node> newAdjList = new ArrayList<>();
-        List<Node> adjList = node.neighbors;
-        for (Node adj : adjList) {
-            Node newAdjNode = newNodeMap.get(adj);
-            newAdjList.add(newAdjNode);
-        }
-        newNode.neighbors = newAdjList;
-    }
-    
-    public void updateList(Node node, boolean[] updateVis, HashMap<Node, Node> newNodeMap) {
-        if (node == null || updateVis[node.val]) return;
-        updateVis[node.val] = true;
-        updateAdjList(node, newNodeMap);
-        List<Node> adjList = node.neighbors;
-        for (Node adj : adjList) {
-            updateList(adj, updateVis, newNodeMap);
-        }
-    }
-    
-    
-    
-    
 }
